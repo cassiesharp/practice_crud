8273fc147c4d06d232d1e672a9261513f8867318
cassie <cassie.sharp@ibm.com>
2020-03-21 17:01:01 -0500

    basic styling and layout

--- src/App.js
+++ src/App.js
@@ -1,34 +1,43 @@
 import React, { useEffect, useState } from 'react';
 import CassieAxios from 'axios';
-import { Tile } from 'carbon-components-react';
-import logo from './logo.svg';
-import './App.scss';
+import { ClickableTile } from 'carbon-components-react';
+import Header from './components/header/Header';
+import './App.css';
 import 'carbon-components/css/carbon-components.min.css';
 
 function App() {
-const [rockets, setRockets] = useState([]);
+  const [rockets, setRockets] = useState([]);
 
   useEffect(() => {
     getAllRockets()
   }, []);
 
-const getAllRockets = () => {
-const rocketURL = 'https://api.spacexdata.com/v3/rockets'
-CassieAxios.get(rocketURL).then(response => {
-  setRockets(response.data)
-})
-}
+  const getAllRockets = () => {
+    const rocketURL = 'https://api.spacexdata.com/v3/rockets'
+    CassieAxios.get(rocketURL).then(response => {
+      setRockets(response.data)
+    })
+  }
 
-  return <>
-  {rockets.map(rocket => {
-    console.log({ rocket })
-    return <Tile>
-      Company: {rocket.company}
-      Country: {rocket.country}
-      <img src={rocket.flickr_images[0]} alt="rocket" />
-    </Tile>
-  })}
-  </>
+  return <div className="rocketContainer">
+    <Header />
+    <div className="rocketRowContainer">
+      {rockets.map(rocket => {
+        console.log({ rocket })
+        return (
+          <ClickableTile href={rocket.wikipedia} target="_blank">
+            <div className="rocketContentContainer">
+              <h4>{rocket.company}</h4>
+              <h4>{rocket.country}</h4>
+            </div>
+            <div>
+              <img src={rocket.flickr_images[0]} className="rocketImages" alt="rocket" />
+            </div>
+          </ClickableTile>
+        )
+      })}
+    </div>
+  </div>
 }
 
 export default App;
---
+++ src/components/header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../../App.css'
+function Header() {
+    return <div className="header">
+        <h1>Welcome to this Space!</h1>
+    </div>
+
+}
+
+export default Header;
